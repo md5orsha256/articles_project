de9565c00805f889922e90d60955b1376761a943
kirill-grehovodov <rikill666@mail.ru>
2021-12-20 19:39:15 +0600

    Создана модель Article и создана миграция

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Article(models.Model):
+    title = models.CharField(max_length=200, null=False, blank=False, verbose_name="Заголовок")
+    author = models.CharField(max_length=200, null=False, blank=False, verbose_name="Автор", default="Unknown")
+    content = models.TextField(max_length=2000, null=False, blank=False, verbose_name="Контент")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="Дата изменения")
+
+    def __str__(self):
+        return f"{self.pk}. {self.author}: {self.title}"
+
+    class Meta:
+        db_table = 'articles'
+        verbose_name = 'Статья'
+        verbose_name_plural = 'Статьи'
