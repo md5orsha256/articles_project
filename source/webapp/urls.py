28bf1b8e9cecfe06a9cb4a7206df1704c599e76d
kirill-grehovodov <rikill666@mail.ru>
2021-12-20 20:37:49 +0600

    Изменена функция create_article_view, добавлено сохранение статьи в базу данных. В шаблоне добавлена ссылка на главную и изменено обращение к свойствам статьи.

--- source/webapp/urls.py
+++ source/webapp/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 
-from webapp.views import index_view, create_articles_view
+from webapp.views import index_view, create_article_view
 
 urlpatterns = [
     path('', index_view),
-    path('articles/add/', create_articles_view)
+    path('articles/add/', create_article_view)
 ]
--- source/webapp/views.py
+++ source/webapp/views.py
@@ -9,13 +9,14 @@ def index_view(request):
     return render(request, 'index.html', {'articles': articles})
 
 
-def create_articles_view(request):
+def create_article_view(request):
     if request.method == 'GET':
         return render(request, 'article_create.html')
     else:
-        context = {
-            "title": request.POST.get('title'),
-            "content": request.POST.get('content'),
-            "author": request.POST.get('author'),
-        }
+        title = request.POST.get('title')
+        content = request.POST.get('content')
+        author = request.POST.get('author')
+        new_article = Article.objects.create(title=title, content=content, author=author)
+        context = {"article": new_article}
+
         return render(request, 'article_view.html', context)
