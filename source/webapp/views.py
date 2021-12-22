0039c25f81454d509a28f850d4134e1591b82b84
kirill-grehovodov <rikill666@mail.ru>
2021-12-22 19:25:48 +0600

    добавлен селект для поля статус при создании новой статьи

--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
-from webapp.models import Article
+from webapp.models import Article, STATUS_CHOICES
 
 
 def index_view(request):
@@ -10,7 +10,7 @@ def index_view(request):
 
 def create_article_view(request):
     if request.method == 'GET':
-        return render(request, 'article_create.html')
+        return render(request, 'article_create.html', {"status_choices": STATUS_CHOICES})
     else:
         title = request.POST.get('title')
         content = request.POST.get('content')
