29f22bf6635557b952f2d192bc833fc5c430ac8d
kirill-grehovodov <rikill666@mail.ru>
2021-12-22 19:28:22 +0600

    при пост запросе создания статьи добавлено получение статуса

--- source/webapp/views.py
+++ source/webapp/views.py
@@ -15,7 +15,11 @@ def create_article_view(request):
         title = request.POST.get('title')
         content = request.POST.get('content')
         author = request.POST.get('author')
-        new_article = Article.objects.create(title=title, content=content, author=author)
+        status = request.POST.get('status')
+        new_article = Article.objects.create(title=title,
+                                             content=content,
+                                             author=author,
+                                             status=status)
         context = {"article": new_article}
 
         return render(request, 'article_view.html', context)
