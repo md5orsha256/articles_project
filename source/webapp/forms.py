340103f3427c2660596578fda96f5510f4943e3a
kirill-grehovodov <rikill666@mail.ru>
2022-01-03 20:58:54 +0600

    реализовано создание статьи с использованием ArticleForm

--- source/webapp/forms.py
+++ source/webapp/forms.py
@@ -1,7 +1,9 @@
 from django import forms
+from django.forms import widgets
 
 
 class ArticleForm(forms.Form):
     title = forms.CharField(max_length=200, required=True, label="Название")
     author = forms.CharField(max_length=200, required=True, label="Автор")
-    content = forms.CharField(max_length=2000, required=True, label="Автор")
+    content = forms.CharField(max_length=2000, required=True, label="Описание",
+                              widget=widgets.Textarea(attrs={"rows": 5, "cols":50}))
--- source/webapp/views.py
+++ source/webapp/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponseRedirect, HttpResponseNotFound, Http404
 from django.urls import reverse
 
+from webapp.forms import ArticleForm
 from webapp.models import Article
 from webapp.utils import article_validate
 
@@ -13,24 +14,21 @@ def index_view(request):
 
 def create_article_view(request):
     if request.method == 'GET':
-        return render(request, 'article_create.html')
+        form = ArticleForm()
+        return render(request, 'article_create.html', {"form": form})
     else:
-        title = request.POST.get('title')
-        content = request.POST.get('content')
-        author = request.POST.get('author')
-        new_article = Article(title=title, content=content, author=author)
-        errors = article_validate(title, content, author)
-        if errors:
-            return render(request, 'article_create.html', {"errors": errors, "article": new_article} )
-        new_article.save()
-        return redirect("article_view", pk=new_article.pk)
+        form = ArticleForm(data=request.POST)
+        if form.is_valid():
+            title = form.cleaned_data.get('title')
+            content = form.cleaned_data.get('content')
+            author = form.cleaned_data.get('author')
+            new_article = Article.objects.create(title=title, content=content, author=author)
+            return redirect("article_view", pk=new_article.pk)
+        return render(request, 'article_create.html', {"form": form})
+
 
 
 def article_view(request, pk):
-    # try:
-    #     article = Article.objects.get(pk=pk)
-    # except Article.DoesNotExist:
-    #     raise Http404
     article = get_object_or_404(Article, pk=pk)
     context = {"article": article}
     return render(request, 'article_view.html', context)
