168a56d624f4a771e245ed90d3bb889089f220a6
kirill-grehovodov <rikill666@mail.ru>
2022-02-10 20:28:30 +0600

    форму для регистрации унаследовали от UserCreationForm, а представление переписали на классовое унаследовавшись от CreateView

--- source/accounts/urls.py
+++ source/accounts/urls.py
@@ -1,12 +1,12 @@
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
-from accounts.views import login_view, logout_view, register_view
+from accounts.views import login_view, logout_view, RegisterView
 
 app_name = 'accounts'
 
 urlpatterns = [
     path('login/', LoginView.as_view(template_name="login.html"), name="login"),
     path('logout/', LogoutView.as_view(), name="logout"),
-    path('registration/', register_view, name="registration"),
+    path('registration/', RegisterView.as_view(), name="registration"),
 ]
--- source/accounts/views.py
+++ source/accounts/views.py
@@ -1,23 +1,44 @@
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
 from django.shortcuts import render, redirect
 
-
 # Create your views here.
+from django.urls import reverse
+from django.views.generic import CreateView
+
 from accounts.forms import MyUserCreationForm
 
 
-def register_view(request):
-    form = MyUserCreationForm()
-    if request.method == "POST":
-        form = MyUserCreationForm(data=request.POST)
-        if form.is_valid():
-            user = form.save()
-            login(request, user)
-            url = request.GET.get("next")
-            if url:
-                return redirect(url)
-            return redirect("webapp:index")
-    return render(request, "registration.html", {"form":form})
+class RegisterView(CreateView):
+    model = User
+    template_name = "registration.html"
+    form_class = MyUserCreationForm
+
+    def form_valid(self, form):
+        user = form.save()
+        login(self.request, user)
+        return redirect(self.get_success_url())
+
+    def get_success_url(self):
+        next_url = self.request.GET.get('next')
+        if not next_url:
+            next_url = self.request.POST.get('next')
+        if not next_url:
+            next_url = reverse('webapp:index')
+        return next_url
+
+# def register_view(request):
+#     form = MyUserCreationForm()
+#     if request.method == "POST":
+#         form = MyUserCreationForm(data=request.POST)
+#         if form.is_valid():
+#             user = form.save()
+#             login(request, user)
+#             url = request.GET.get("next")
+#             if url:
+#                 return redirect(url)
+#             return redirect("webapp:index")
+#     return render(request, "registration.html", {"form":form})
 
 
 def login_view(request):
