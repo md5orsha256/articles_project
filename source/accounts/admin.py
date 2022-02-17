17290828c0d3eb6cf1c12b24fa586985cd97081e
Stepanov Dmitry <stpnv.dmtry@gmail.com>
2022-02-17 20:10:16 +0600

    Создана модель Profile     которая имеет связь 1-к-1 с моделью пользователя.     Сгенерированы миграции     Модель добавлена в админ панель

--- source/accounts/admin.py
+++ source/accounts/admin.py
@@ -1,3 +1,20 @@
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin
 from django.contrib import admin
 
-# Register your models here.
+from accounts.models import Profile
+
+User = get_user_model()
+
+
+class ProfileInline(admin.StackedInline):
+    fields = ("birth_date", "avatar")
+    model = Profile
+
+
+class UserProfileAdmin(UserAdmin):
+    inlines = (ProfileInline,)
+
+
+admin.site.unregister(User)
+admin.site.register(User, UserProfileAdmin)
--- source/accounts/models.py
+++ source/accounts/models.py
@@ -1,3 +1,31 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(
+        get_user_model(),
+        related_name="profile",
+        verbose_name="Профиль",
+        on_delete=models.CASCADE,
+    )
+
+    birth_date = models.DateField(
+        null=True,
+        blank=False,
+        verbose_name="Дата рождения",
+    )
+
+    avatar = models.ImageField(
+        verbose_name="Аватар",
+        upload_to="avatars/",
+        null=True,
+        blank=False
+    )
+
+    class Meta:
+        verbose_name = "Профиль"
+        verbose_name_plural = "Профили"
+
+    def __str__(self) -> str:
+        return f"Профиль: {self.user.username}. {self.id}"
