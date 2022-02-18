b890554d4284254e68636b6500a5f958efac81c5
Stepanov Dmitry <stpnv.dmtry@gmail.com>
2022-02-18 12:53:32 +0600

    Добавлена страница просмотра профиля пользователя

--- source/accounts/urls.py
+++ source/accounts/urls.py
@@ -1,11 +1,12 @@
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
-from accounts.views import login_view, logout_view, RegisterView
+from accounts.views import RegisterView, UserProfileView
 
 app_name = 'accounts'
 
 urlpatterns = [
+    path("<int:pk>/", UserProfileView.as_view(), name="user-profile"),
     path('login/', LoginView.as_view(template_name="login.html"), name="login"),
     path('logout/', LogoutView.as_view(), name="logout"),
     path('registration/', RegisterView.as_view(), name="registration"),
--- source/accounts/views.py
+++ source/accounts/views.py
@@ -1,12 +1,13 @@
-from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth import authenticate, login, logout, get_user_model
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.models import User
+from django.core.paginator import Paginator
 from django.shortcuts import render, redirect
-
-# Create your views here.
 from django.urls import reverse
-from django.views.generic import CreateView
+from django.views.generic import CreateView, DetailView
 
 from accounts.forms import MyUserCreationForm
+from accounts.models import Profile
 
 
 class RegisterView(CreateView):
@@ -16,6 +17,7 @@ class RegisterView(CreateView):
 
     def form_valid(self, form):
         user = form.save()
+        Profile.objects.create(user=user)
         login(self.request, user)
         return redirect(self.get_success_url())
 
@@ -26,19 +28,6 @@ class RegisterView(CreateView):
         if not next_url:
             next_url = reverse('webapp:index')
         return next_url
-
-# def register_view(request):
-#     form = MyUserCreationForm()
-#     if request.method == "POST":
-#         form = MyUserCreationForm(data=request.POST)
-#         if form.is_valid():
-#             user = form.save()
-#             login(request, user)
-#             url = request.GET.get("next")
-#             if url:
-#                 return redirect(url)
-#             return redirect("webapp:index")
-#     return render(request, "registration.html", {"form":form})
 
 
 def login_view(request):
@@ -60,3 +49,27 @@ def login_view(request):
 def logout_view(request):
     logout(request)
     return redirect('webapp:index')
+
+
+class UserProfileView(LoginRequiredMixin, DetailView):
+    model = get_user_model()
+    template_name = "profile.html"
+    context_object_name = "user_object"
+    paginate_related_by = 5
+    paginate_related_orphans = 0
+
+    def get_context_data(self, **kwargs):
+        paginator = Paginator(
+            self.get_object().articles.all(),
+            self.paginate_related_by,
+            self.paginate_related_orphans,
+        )
+
+        page_number = self.request.GET.get('page', 1)
+        page = paginator.get_page(page_number)
+
+        kwargs['page_obj'] = page
+        kwargs['articles'] = page.object_list
+        kwargs['is_paginated'] = page.has_other_pages()
+
+        return super(UserProfileView, self).get_context_data(**kwargs)
