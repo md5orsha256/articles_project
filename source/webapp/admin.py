f895aa467c0b7836cead673888c13edc0639daa6
Stepanov Dmitry <stpnv.dmtry@gmail.com>
2022-02-21 18:35:47 +0600

    Тэги добавлены в панель администратора

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -1,15 +1,24 @@
 from django.contrib import admin
 
-from webapp.models import Article, Comment
+from webapp.models import Article, Comment, Tag
 
 
 class ArticleAdmin(admin.ModelAdmin):
     list_display = ['id', 'title', 'author', 'created_at']
     list_filter = ['author']
     search_fields = ['title', 'content']
-    fields = ['title', 'author', 'content', 'created_at', 'updated_at']
+    fields = ['title', 'author', 'content', 'tags', 'created_at', 'updated_at']
+    readonly_fields = ['created_at', 'updated_at']
+
+
+class TagAdmin(admin.ModelAdmin):
+    list_display = ['name']
+    list_filter = ['name']
+    search_fields = ['name']
+    fields = ['name', "created_at", "updated_at"]
     readonly_fields = ['created_at', 'updated_at']
 
 
 admin.site.register(Article, ArticleAdmin)
 admin.site.register(Comment)
+admin.site.register(Tag, TagAdmin)
