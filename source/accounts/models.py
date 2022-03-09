95a46a1ebe02452ea983c6f2805587a1c510690f
Stepanov Dmitry <stpnv.dmtry@gmail.com>
2022-03-09 20:47:13 +0600

    \#2 Создали модель для хранения токенов подтверждения адресов email

--- source/accounts/models.py
+++ source/accounts/models.py
@@ -1,10 +1,17 @@
+from datetime import timedelta
+from secrets import token_urlsafe
+
 from django.contrib.auth import get_user_model
+from django.utils import timezone
+
 from django.db import models
+
+User = get_user_model()
 
 
 class Profile(models.Model):
     user = models.OneToOneField(
-        get_user_model(),
+        User,
         related_name="profile",
         verbose_name="Профиль",
         on_delete=models.CASCADE,
@@ -29,3 +36,14 @@ class Profile(models.Model):
 
     def __str__(self) -> str:
         return f"Профиль: {self.user.username}. {self.id}"
+
+
+class EmailConfirmationToken(models.Model):
+    TOKEN_LIFETIME_SECONDS = 2 * 60 * 60
+
+    token = models.CharField(null=False, max_length=255, unique=True, default=token_urlsafe)
+    created_at = models.DateField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=False)
+
+    def is_expired(self):
+        return self.created_at + timedelta(seconds=self.TOKEN_LIFETIME_SECONDS) < timezone.now()
